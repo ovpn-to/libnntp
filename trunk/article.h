6459e251792d26de920d65a865bec8ccce27e57b
martijntje <martijntje@89b57d99-8bc4-ba6c-f400-9e555812c519>
2011-02-22 20:13:18 +0000

    Add license notices to every file

--- trunk/article.h
+++ trunk/article.h
@@ -1,5 +1,22 @@
-#ifndef ARTICLE_H
+/**
+  * This file is part of libnntp.
+  *
+  * libnntp is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * libnntp is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with libnntp. If not, see <http://www.gnu.org/licenses/>.
+  */
+
 #define ARTICLE_H 1
+#ifndef ARTICLE_H
 
 #include "decoded_article.h"
 #include <boost/intrusive_ptr.hpp>
